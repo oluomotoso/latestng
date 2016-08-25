20201e33707cdb5bab7d28f808e37d5fbb6a61fe
OMOTOSO OLUMIDE <OMOTOSO OLUMIDE>
2016-08-25 12:27:18 +0100

    Edited edge response for bulk delete
    Signed-off-by: OMOTOSO OLUMIDE <OMOTOSO OLUMIDE>

--- app/facebook_edge.php
+++ app/facebook_edge.php
@@ -8,9 +8,13 @@ class facebook_edge extends Model
 {
     //
     protected $table = 'facebook_edges';
-    protected $fillable = ['name', 'facebook_id', 'facebook_account_owner','edge_type','response'
+    protected $fillable = ['name', 'facebook_id', 'facebook_account_owner', 'edge_type', 'response'
 
     ];
 
+    public function facebook_account()
+    {
+       return $this->belongsTo('newsbook\facebook_account','facebook_account_owner');
+    }
 
 }
